9050d2e203d25d69cbdff510561997c4021df93c
DavidLapi <davidvaamonde.bueno@gmail.com>
2026-04-23 20:41:50 +0200

    feat: ajustes laboratorio

--- Python/02_Bucles_y_Secuencias/laboratorio/main.py
+++ Python/02_Bucles_y_Secuencias/laboratorio/main.py
@@ -1,16 +1,18 @@
 # Funcion number_pattern
 def number_pattern(n):
     if not isinstance(n, int):
-        return "Argument must be an integer value."
+        return f"Argument {n} must be an integer value."
     elif n < 1:
-        return "Argument must be an integer greater than 0."
+        return f"Argument {n} must be an integer greater than 0."
     else:
         lista = []
         for num in range(1, n+1):
-            cadena = str(num)
-            lista.append(cadena)
+            texto = str(num)
+            lista.append(texto)
         string = " ".join(lista)
         return string
 
+print(number_pattern("Cachopo"))
+print(number_pattern(-2))
 print(number_pattern(4))
 print(number_pattern(12))
